191d9cd52b872f0717de6299dd46109da5009680
dtork@dolores <dtorkelson@gmail.com>
2022-03-22 18:46:52 +0000

    use timestamp that comes back from query

--- pro.py
+++ pro.py
@@ -33,12 +33,15 @@ class CoinbaseProClient(cbpro.PublicClient):
             print(chunk)
 
             fetched_quotes = [self._get_quote(coin, date) for coin in chunk]
-            quote_update = {coin: quote for coin, quote in zip(chunk, fetched_quotes)}
+            quote_update = {
+                coin: {'price': quote, 'time': time} for coin, (quote, time) in zip(chunk, fetched_quotes)
+            }
             quote_mappings.update(quote_update)
             sleep(2)
         return quote_mappings
 
-    def _get_quote(self, currency: str, date: str):
+    def _get_quote(self, currency: str, date: str) -> tuple:
+        """Return quote and timestamp for given coin."""
         for base_currency in ['USD', 'USDC', 'USDT']:
             currency_pair = f'{currency}-{base_currency}'
             if date:
@@ -51,7 +54,7 @@ class CoinbaseProClient(cbpro.PublicClient):
                 quote_data =  self.get_product_ticker(product_id=currency_pair)
 
             try:
-                return quote_data['price']
+                return quote_data['price'], quote_data['time']
             except KeyError:
                 continue
         return None
@@ -63,10 +66,13 @@ class CoinbaseProClient(cbpro.PublicClient):
         Uses the first record regardless of how many candles are returned, assuming
         that granularity was set to 86400 (one per day) and the date range was a
         single date.
+
+        Converts the timestamp from unix seconds to a datetime object.
         """
         response_fields = 'time low high open close volume'.split()
         candle_data = {k: v for k, v in zip(response_fields, response[0])}
         candle_data['price'] = candle_data['close']
+        candle_data['time'] = datetime.fromtimestamp(candle_data['time'])
         return candle_data
 
 
@@ -82,9 +88,8 @@ def main():
     cb_client = CoinbaseProClient()
     mapped_quotes = cb_client.get_quotes(currencies=input_currencies, date=quote_date)
 
-    label_date = quote_date if quote_date else datetime.now().strftime('%Y-%m-%d')
-    df_quotes = convert_quotes_to_df(quotes=mapped_quotes, date=label_date)
-    write_df_to_file(df_quotes, date=label_date)
+    df_quotes = convert_quotes_to_df(quotes=mapped_quotes)
+    write_df_to_file(df_quotes)
 
 
 def parse_args():
@@ -121,17 +126,16 @@ def get_input_currency_list(filepath: str) -> list:
         return clean_lines
 
 
-def convert_quotes_to_df(quotes: dict, date: str) -> pd.DataFrame:
+def convert_quotes_to_df(quotes: dict) -> pd.DataFrame:
     df = pd.DataFrame.from_dict(quotes, orient='index').reset_index()
-    df.columns = ['currency', 'price_usd']
-    df['date'] = date
+    df.columns = ['currency', 'price_usd', 'timestamp']
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
     return df
 
 
-def write_df_to_file(df: pd.DataFrame, date: str):
-    date_format = '%Y-%m-%d'
+def write_df_to_file(df: pd.DataFrame):
     ts_format = '%Y-%m-%d_%H%M_%S'
-    timestamp = datetime.strptime(date, date_format)
+    timestamp = df['timestamp'].iloc[0]
     ts_formatted = timestamp.strftime(ts_format)
 
     outname = f'crypto_prices_as_of_{ts_formatted}.csv'
