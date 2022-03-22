26e2fea9fdbdd5f5c5dd44c28e6e8042fa52309a
dtork@dolores <dtorkelson@gmail.com>
2022-03-22 16:29:20 +0000

    build in CLI arg for historic date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yaml 
 import argparse
+from datetime import datetime
 from coinbase.wallet.client import Client
 
 CURRENCY_CODE = 'USD'
@@ -8,18 +9,43 @@ CURRENCY_CODE = 'USD'
 def main():
     args = parse_args()
     currency = args.currency
+    try:
+        quote_date = validate_passed_date(passed_date=args.date)
+    except TypeError:
+        quote_date = None
 
     secrets_file = 'secrets.yaml'
     secrets = read_secrets(secrets_file)
 
-    quote = get_quote(currency=currency, secrets=secrets)
+    quote = get_quote(currency=currency, secrets=secrets, date=quote_date)
     print_answer(quote)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Get quotes for cryptocurrencies (in USD).')
     parser.add_argument('currency', help='cryptocurrency symbol')
+    parser.add_argument('-d', '--date', help='as-of date for quote')
     return parser.parse_args()
+
+
+def validate_passed_date(passed_date: str) -> str:
+    """Ensure the date passed as an arg conforms to the expected format.
+
+    If it does not, fill in with today's date. 
+
+    Raises:
+        TypeError: If no date is passed via command line, and a current quote is desired.
+
+    """
+    if not passed_date:
+        raise TypeError
+    expected_format = '%Y-%m-%d'
+    try:
+        datetime.strptime(passed_date, expected_format)
+    except ValueError:
+        return datetime.today().strftime(expected_format)
+    else:
+        return passed_date
 
 
 def read_secrets(filename: str) -> dict:
@@ -27,10 +53,15 @@ def read_secrets(filename: str) -> dict:
         secrets = yaml.safe_load(f)
     return secrets
 
-def get_quote(currency: str, secrets: dict) -> dict:
+
+def get_quote(currency: str, secrets: dict, date: str=None) -> dict:
     """Get the quote of a currency.
 
-    If historic spot price needed, add `date` parameter to `get_spot_price` as YYYY-MM-DD
+    Args:
+        currency: Three-letter currency symbol/ticker to query.
+        secrets: Key and password for Coinbase API.
+        date: Date for which to fetch historic quote, as YYYY-MM-DD. Optional.
+            If no date is passed, the current quote is fetched.
 
     Returns:
         API response dict, with the fields
@@ -44,16 +75,14 @@ def get_quote(currency: str, secrets: dict) -> dict:
 
     currency = currency.upper()
     currency_pair = f"{currency}-{CURRENCY_CODE}"
-    quote = client.get_spot_price(currency_pair=currency_pair)
+    quote = client.get_spot_price(currency_pair=currency_pair, date=date)
+    quote['date'] = datetime.today().strftime('%Y-%m-%d')
     return quote
 
 
 def print_answer(response: dict):
     """Pretty-print the quote."""
-    amount = response['amount']
-    base = response['base']
-    currency = response['currency']
-    print(f"Current price of {currency} is {amount} {base}")
+    print("{date} price of {currency}: {amount} {base}".format(**response))
 
 
 if __name__ == '__main__':
